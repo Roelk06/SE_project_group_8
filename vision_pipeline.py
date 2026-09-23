eec64a0de471f5ef1d74da1a9f6e514c972d4225
Roelk06 <r.j.kuipers@tilburguniversity.edu>
2026-09-23 20:54:15 +0200

    Add vision preprocessing pipeline

--- vision_pipeline.py
+++ vision_pipeline.py
@@ -30,6 +30,20 @@ for folder_name, label in labels_map.items():
         x_list.append(image)
         y_list.append(label)
 
+x = np.array(x_list, dtype=np.float32)
+y = np.array(y_list)
 
+x = x / 255.0
+x = x[..., np.newaxis]           
+x = np.transpose(x, (0, 3, 1, 2))   
 
+x_train, x_temp, y_train, y_temp = train_test_split(x, y, test_size=0.3, stratify=y, random_state=42)
+x_val, x_test, y_val, y_test = train_test_split(x_temp, y_temp, test_size=0.5, stratify=y_temp, random_state=42)
+
+np.save(os.path.join(processed_dir, "x_train.npy"), x_train)
+np.save(os.path.join(processed_dir, "y_train.npy"), y_train)
+np.save(os.path.join(processed_dir, "x_val.npy"), x_val)
+np.save(os.path.join(processed_dir, "y_val.npy"), y_val)
+np.save(os.path.join(processed_dir, "x_test.npy"), x_test)
+np.save(os.path.join(processed_dir, "y_test.npy"), y_test)
 
